39fabe8b179354206838033bc919fec0018b8ee3
EvaldoRibeiro <65911994+EvaldoRibeiro@users.noreply.github.com>
2020-08-14 17:39:42 -0300

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/evaldo/cursomc/resources/utils/URL.java
@@ -0,0 +1,31 @@
+package com.evaldo.cursomc.resources.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class URL {
+	
+	public static String decodeParam(String s) {
+		try {
+			return URLDecoder.decode(s, "UTF-8");
+		}
+		catch (UnsupportedEncodingException e) {
+			return "";
+		}
+	}
+	
+	public static List<Integer> decodeIntList(String s){
+		String [] vet = s.split(",");
+//		List<Integer> list = new ArrayList<>();
+//		for (int i=0; i<vet.length; i++) {
+//			Integer.parseInt(vet[i]); 
+//			list.add(Integer.parseInt(vet[1]));
+//		}
+//		return list;
+		return Arrays.asList(s.split(",")).stream().map(x -> Integer.parseInt(x)).collect(Collectors.toList());
+	}
+
+}
