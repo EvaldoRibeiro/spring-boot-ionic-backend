56734ca907e15e3742bd1bbd44c3173a43e457f5
EvaldoRibeiro <65911994+EvaldoRibeiro@users.noreply.github.com>
2020-08-17 21:26:42 -0300

    Criando profile de desenvolvimento erro_de_fuso horário

--- src/main/java/com/evaldo/cursomc/resources/PedidoResource.java
+++ src/main/java/com/evaldo/cursomc/resources/PedidoResource.java
@@ -13,9 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.evaldo.cursomc.domain.Categoria;
 import com.evaldo.cursomc.domain.Pedido;
-import com.evaldo.cursomc.dto.CategoriaDTO;
 import com.evaldo.cursomc.services.PedidoService;
 
 @RestController
