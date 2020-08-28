977c3ab8d7af905f14c1ce301577f1c3892bb4b7
EvaldoRibeiro <65911994+EvaldoRibeiro@users.noreply.github.com>
2020-08-28 12:33:48 -0300

    Adicionando senha a Cliente

--- src/main/java/com/evaldo/cursomc/domain/Cliente.java
+++ src/main/java/com/evaldo/cursomc/domain/Cliente.java
@@ -32,6 +32,9 @@ public class Cliente implements Serializable {
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
+	
+	@JsonIgnore
+	private String senha;
 
 	
 	
@@ -49,13 +52,14 @@ public class Cliente implements Serializable {
 	public Cliente() {
 	}
 
-	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.email = email;
 		this.cpfOuCnpj = cpfOuCnpj;
 		this.tipo = (tipo == null) ? null : tipo.getCod(); // tipo ternario
+		this.senha = senha;
 	}
 
 	public Integer getId() {
@@ -97,6 +101,13 @@ public class Cliente implements Serializable {
 	public void setTipo(TipoCliente tipo) {
 		this.tipo = tipo.getCod();
 	}
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 
 	public List<Endereco> getEnderecos() {
 		return enderecos;
@@ -121,6 +132,7 @@ public class Cliente implements Serializable {
 	public void setPedidos(List<Pedido> pedidos) {
 		this.pedidos = pedidos;
 	}
+	
 
 	@Override
 	public int hashCode() {
@@ -147,4 +159,5 @@ public class Cliente implements Serializable {
 		return true;
 	}
 
+	
 }
--- src/main/java/com/evaldo/cursomc/services/DBService.java
+++ src/main/java/com/evaldo/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.evaldo.cursomc.domain.Categoria;
@@ -33,6 +34,8 @@ import com.evaldo.cursomc.repositories.ProdutoRepository;
 @Service
 public class DBService {
 	
+	@Autowired
+	private BCryptPasswordEncoder pe;
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	@Autowired
@@ -111,7 +114,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Davi Gomes Moraes", "maria@gmail.com", "56108648015", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Davi Gomes Moraes", "maria@gmail.com", "56108648015", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		
 		cli1.getTelefones().addAll(Arrays.asList("32113369", "34988040993"));
 		
