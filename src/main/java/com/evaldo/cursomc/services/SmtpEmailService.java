b1bfb57aa4a5c07e5b5d3b766a517b99c93e5b26
EvaldoRibeiro <65911994+EvaldoRibeiro@users.noreply.github.com>
2020-08-27 22:38:08 -0300

    Email HTML

--- src/main/java/com/evaldo/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/evaldo/cursomc/services/SmtpEmailService.java
@@ -1,14 +1,20 @@
 package com.evaldo.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService{
 
 	@Autowired
 	private MailSender mailSender;
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
 	
 	private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(SmtpEmailService.class);
 	
@@ -16,6 +22,15 @@ public class SmtpEmailService extends AbstractEmailService{
 	public void sendEmail(SimpleMailMessage msg) {
 		LOG.info("Enviando email ...");
 		mailSender.send(msg);
+		LOG.info(msg.toString());
+		LOG.info("Email enviado");
+		
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Enviando email HTML ...");
+		javaMailSender.send(msg);
 		LOG.info(msg.toString());
 		LOG.info("Email enviado");
 		
